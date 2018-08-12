1b8e2db22a70d1c3365bf2df9dd26a8f0dce52b9
david <david.journo.31@gmail.com>
2018-08-12 22:18:34 +0200

    partials

---
+++ parts/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Randonnées</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.3.3/dist/semantic.min.css">
+    <link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+</head>
+<body>
+<?php
+include 'menu.php';
+?>
---
+++ parts/menu.php
@@ -0,0 +1,8 @@
+<div class="ui secondary top fixed main menu">
+<a class="item" href="/">
+    Read
+  </a>
+  <a class="ui item" href="?page=create">
+    Create
+  </a>
+</div>
